9c5a19379fe2b54491773b79cc06bbdbd32b5d23
ankitkings <49301724+ankitkings@users.noreply.github.com>
2024-01-30 10:26:13 +0000

    varialble types

---
+++ 001_basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 12;
+var accountEmail = "ankit@gmail.com";
+let accountActive = true;
+let accountDetails = null;
+let accountCode;
+let accountScholarNumber =  19384974889436834838783n
+
+// console.log(accountId);
+// console.log(accountEmail);
+console.table([accountId, accountEmail, accountActive, accountDetails, accountCode, accountScholarNumber])
+
+// console.log(typeof accountId);
+// console.log(typeof accountEmail);
+// console.log(typeof accountActive);
+// console.log(typeof accountDetails);
+// console.log(typeof accountCode);accountScholarNumber
+console.log(typeof accountScholarNumber);
