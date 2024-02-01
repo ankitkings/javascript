83a972eb00c83e5c4181e3db61031eef2ef70811
ankitkings <49301724+ankitkings@users.noreply.github.com>
2024-02-01 08:25:12 +0000

    functions

---
+++ 003_basics/001_functions.js
@@ -0,0 +1,57 @@
+// function myName(){
+//     console.log("A");
+//     console.log("n");
+//     console.log("k");
+//     console.log("i");
+//     console.log("t");
+// }
+// myName()
+
+// function sum(number1, number2){
+//     // console.log(number1+number2)
+//     return number1+number2
+// }
+
+// const add = sum(3,2)
+// console.log(add);
+
+// function loginMessage(username){
+//   if (undefined){
+//     console.log("Please enter usename")
+//     return
+//   }
+//   console.log( `Welcome, ${username}`)
+// }
+
+// function loginMessage(username){
+//   if (!username){
+//     console.log("Please enter usename")
+//     return
+//   }
+//   console.log( `Welcome, ${username}`)
+// }
+
+// loginMessage("Ankit")
+// loginMessage()
+
+
+// function totalPaymentOfCart(...pay2){
+//   return pay2
+// }
+
+// console.log(totalPaymentOfCart(120,345,444));
+
+// function displayObject(user){
+//   return `User name is ${user.name} and Price is ${user.price}`
+// }
+
+// console.log(displayObject({
+//   name: "Mayank",
+//   price: 200
+// }))
+
+
+function listArray(myArray){
+  return myArray[0]
+}
+ console.log(listArray([233,33,34,2,4,2]));
